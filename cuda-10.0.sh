a761bde79793c1d0cc69ae22fd4bbb6dca62c305
Chih-Hung Tseng <chtseng@itri.org.tw>
2019-03-21 10:19:22 +0800

    Update cuda-10.0.sh
    LD_LIBRARY_PATH has a system-wide affect when running a problem. It could be a source of bug and hard to debug.
    Avoid setting it in .bashrc.

--- cuda-10.0.sh
+++ cuda-10.0.sh
@@ -11,6 +11,4 @@ sudo apt-get -y install cuda
 # set environment variables
 echo "export PATH=${PATH}:/usr/local/cuda/bin" >> ~/.bashrc
 echo "export CUDA_HOME=${CUDA_HOME}:/usr/local/cuda:/usr/local/cuda" >> ~/.bashrc
-echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda/lib64" >> ~/.bashrc
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/extras/CUPTI/lib64" >> ~/.bashrc
 source ~/.bashrc
