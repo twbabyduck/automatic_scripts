480d52f72d29e9260283f1b0632087e94a2a3dec
chtseng <chtseng@itri.org.tw>
2019-03-21 16:56:46 +0800

    Revert "Update cuda-10.0.sh"
    This reverts commit a761bde79793c1d0cc69ae22fd4bbb6dca62c305.

--- cuda-10.0.sh
+++ cuda-10.0.sh
@@ -11,4 +11,6 @@ sudo apt-get -y install cuda
 # set environment variables
 echo "export PATH=${PATH}:/usr/local/cuda/bin" >> ~/.bashrc
 echo "export CUDA_HOME=${CUDA_HOME}:/usr/local/cuda:/usr/local/cuda" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda/lib64" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/extras/CUPTI/lib64" >> ~/.bashrc
 source ~/.bashrc
