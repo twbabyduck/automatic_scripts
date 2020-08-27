9bb3fe2afe0b64f54c805016cefa7554aafa8da8
Tzu Lin, Huang <tzu.lin.huang@unikie.com>
2020-08-27 11:04:52 +0300

    Update eigen 3.3.7 URL

--- eigen-3.3.7.sh
+++ eigen-3.3.7.sh
@@ -1,4 +1,4 @@
-cd && wget http://bitbucket.org/eigen/eigen/get/3.3.7.tar.gz #Download Eigen
-mkdir eigen && tar --strip-components=1 -xzvf 3.3.7.tar.gz -C eigen #Decompress
+cd && wget https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.gz #Download Eigen
+mkdir eigen && tar --strip-components=1 -xzvf eigen-3.3.7.tar.gz -C eigen #Decompress
 cd eigen && mkdir build && cd build && cmake .. && make && sudo make install #Build and install
-cd && rm -rf 3.3.7.tar.gz && rm -rf eigen #Remove downloaded and temporary files
+cd && rm -rf eigen-3.3.7.tar.gz && rm -rf eigen #Remove downloaded and temporary files
