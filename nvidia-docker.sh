08aecae3c1934c350009f3390addfe0224c763f0
Tzu Lin, Huang <twbabyduck@gmail.com>
2021-05-26 22:33:31 +0800

    Update NVIDIA Docker Script

--- nvidia-docker.sh
+++ nvidia-docker.sh
@@ -16,10 +16,11 @@ sudo add-apt-repository \
 sudo apt-get update && sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 # Add the package repositories
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update && sudo apt-get install -y nvidia-docker2
 
 sudo pkill -SIGHUP dockerd
 sudo usermod -aG docker $USER
